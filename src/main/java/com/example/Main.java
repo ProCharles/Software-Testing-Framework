3d46e4405aa6ab2a539e99419be5f947a216c10d
Charles Mbonani <65178528+ProCharles@users.noreply.github.com>
2026-07-21 15:02:28 +0200

    Navigate to the website

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a new instance of the Chrome browse
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the SauceDemo website
+        driver.get("https://www.saucedemo.com/");
+
+    }
+}
