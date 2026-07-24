572d228b57f7e436709cfb7d0b184b63c0d505c1
Charles Mbonani <65178528+ProCharles@users.noreply.github.com>
2026-07-24 14:01:58 +0200

    locators

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,19 +1,51 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Create a new instance of the Chrome browser
         WebDriver driver = new EdgeDriver();
 
         // Navigate to the SauceDemo website
         driver.get("https://www.saucedemo.com/");
+
+        /// Html/DOm element locators
+        /// Name
+        /// ID
+        /// Class
+        /// CSS Selectors
+        /// Xpath Selectors
+
+
+        //Store test data in variables
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+
+        //locate username field and enter value
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys(username);
+
+
+        //locate password field and enter value
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys(password);
+
+
+        //locate login button and click it
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
+        loginButton.click();
+
+        Thread.sleep(5000);
+
 
         /// Close the browser session and clean up resources
         ///This terminates the WebDriver instance and closes all associated windows.
